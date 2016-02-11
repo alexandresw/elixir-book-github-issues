7a39aa4e9306fde2a52f6e188db1aa5c9fdf150c
Alexandre Watanabe <alexandresw@gmail.com>
2016-02-11 13:56:35 -0500

    added github fetch

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -8,7 +8,9 @@ defmodule Issues.CLI do
 		of the last _n_ issues in a github project
 		"""
 		def run(argv) do 
-			parse_args(argv)
+			argv
+			|> parse_args
+			|> process
 		end
 
 		@doc """
@@ -34,6 +36,17 @@ defmodule Issues.CLI do
 
 				_ -> :help
 			end
+		end
+
+		def process(:help) do
+			IO.puts """
+				usage: issues <user> <project> [ count | #{@default_count} ]
+			"""
+			System.halt(0)
+		end
+
+		def process({user, project, _count}) do
+			Issues.GithubIssues.fetch(user, project)
 		end
 
 
