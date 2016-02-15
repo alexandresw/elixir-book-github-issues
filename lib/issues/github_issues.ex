46c890f29b97348b63c57a9d7da98263705c5b35
Alexandre Watanabe <alexandresw@gmail.com>
2016-02-15 00:22:28 -0500

    transformation: convert response

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -1,5 +1,6 @@
 defmodule Issues.GithubIssues do
-	@user_agent [ {"User-agent", "Elixir alexandresw@gmail.com"}]
+	@user_agent [ {"User-agent", "Alexandre alexandresw@gmail.com"}]
+	@github_url Application.get_env(:issues, :github_url)
 
 	def fetch(user, project) do
 		issues_url(user, project)
@@ -8,9 +9,17 @@ defmodule Issues.GithubIssues do
 	end
 
 	def issues_url(user, project) do
-		"https://api.github.com/repos/#{user}/#{project}/issues"
+		"#{@github_url}/repos/#{user}/#{project}/issues"
 	end
 
-	def handle_response({ :ok, %{status_code: 200, body: body}}), do: { :ok, body}
-	def handle_response({ _, %{status_code: _, body: body}}), do: { :error, body }
+	def handle_response({ :ok, %{status_code: 200, body: body}})
+	do 
+		{ :ok, Poison.Parser.parse!(body) }
+	end
+	
+	def handle_response({ _, %{status_code: ___, body: body}})
+	do 
+		{ :error, Poison.Parser.parse!(body) }
+	end
+
 end
